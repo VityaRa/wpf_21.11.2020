9e8f8561e25d8c3541e39a4754c899dec5c2ae71
VityaRa <68313227+VityaRa@users.noreply.github.com>
2020-11-24 12:35:16 +0300

    NewProj

---
+++ CsharpWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TEXT
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Person> Persons = new List<Person>();
+            PersonDataGrid.ItemsSource = Persons;
+        }
+
+
+        public class Person
+        {
+            public string firstName { get; set; }
+            public string surName { get; set; }
+            public string othcestvo { get; set; }
+            public string position { get; set; }
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            new Window1().Show();
+            this.Close();
+            
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            new Window1().Show();
+        }
+
+
+
+    }
+
+}
